3735fca37bf50aee5049fcba1987afd41b26f3f5
HeritageHigherInstitute <156737269+HeritageHigherInstitute@users.noreply.github.com>
2024-01-20 19:19:53 +0100

    Update puppyh.js

--- puppyh.js
+++ puppyh.js
@@ -45,3 +45,28 @@ if(buttonclicked) {
         hoursdiv.classList.add('active'); 
     })
 }
+
+//this is a functio to submit a mail
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value ,
+        email: document.getElementById("email").value ,
+        message: document.getElementById("message").value,
+    };
+
+const serviceId = "service_gz2cxx5";
+const templateId = "template_edbs38h";
+
+emailjs
+.send(serviceId, templateId, params)
+.then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("you message has been sent successfuly"); 
+        }
+)
+.catch((err) => console.log(err));
+}
